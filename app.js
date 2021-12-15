837a4d0ba47e65d29828fefbc13c6f57a5b617d0
Abdulrahman797 <70597324+Abdulrahman797@users.noreply.github.com>
2021-12-15 23:05:55 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const hamburger_menu = document.querySelector(".hamburger-menuM");
+const container = document.querySelector(".containerM");
+const links = document.querySelector(".linksM");
+const ul = document.querySelector(".linksMUl");
+const page = document.querySelector(".page-inner");
+ul.style.display = "none";
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+  function myFunction(x) {
+    if (x.matches && !container.classList.contains("active")) {
+      // If media query matches
+      links.style.height = "8vh";
+      page.style.display = "flex";
+    } else if (container.classList.contains("active")) {
+      links.style.height = "100vh";
+      page.style.display = "none";
+    } else {
+      links.style.height = "14vh";
+      page.style.display = "flex";
+    }
+    if (x.matches) {
+      let progress = document.getElementById("progressbar");
+      let totalHeight = document.body.scrollHeight - window.innerHeight;
+      window.onscroll = function () {
+        let progressHeight = (window.pageYOffset / totalHeight) * 100;
+        progress.style.height = progressHeight + "%";
+      };
+    }
+  }
+
+  if (container.classList.contains("active")) {
+    links.style.backgroundColor = "#1d1d1d";
+    links.style.height = "100vh";
+    ul.style.display = "flex";
+  } else {
+    links.style.backgroundColor = "initial";
+    links.style.height = "14vh";
+    ul.style.display = "none";
+    page.style.display = "flex";
+  }
+  var x = window.matchMedia("(max-width: 1368px)");
+  myFunction(x); // Call listener function at run time
+  x.addEventListener(myFunction);
+});
